ddce19c2477d7e78ef7f595dd4b3300e20d150ae
Nikolay Korshunov <Metallurg.IT@Gmail.com>
2015-05-18 03:43:22 +0300

    CSS - /assets/application.css

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,20 +1,24 @@
 require 'spec_helper'
 
 describe "Static pages" do
-
+##################################################################
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
-
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
-
+##################################################################
   describe "Help page" do
 
     it "should have the content 'Help'" do
@@ -27,7 +31,7 @@ describe "Static pages" do
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
-
+##################################################################
   describe "About page" do
 
     it "should have the content 'About Us'" do
@@ -40,4 +44,5 @@ describe "Static pages" do
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+##################################################################
 end
