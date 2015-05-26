4037db26afddc93ac88be7751680c33e655f0531
Nikolay Korshunov <Metallurg.IT@Gmail.com>
2015-05-26 04:52:49 +0300

    REF тестов

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -24,6 +24,14 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      # Проверка вывода текстового списка ошибок при ошибочной регистрации
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
     end
 
 # Проверка регистрации пользователя с заполненными формами
@@ -33,6 +41,15 @@ describe "User pages" do
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
+      end
+
+      # Проверка alert приветствия при удачной регистрации пользователя
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Добро') }
       end
 
       # Счётчик пользователей увеличивается на 1 пункт при регистрации
