20c723b6dcc30193bb86b5263dcec830466401cc
Alexander Borovykh <immaculate.pine@gmail.com>
2015-05-31 20:29:56 +0200

    Events pages updated

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,10 @@
 class EventsController < ApplicationController
+  # GET /events
   def index
-    @events = Event.order(started_at: :desc)
+    @events = Event.order(started_at: :asc)
   end
 
+  # GET /events/:id
   def show
     @event = Event.find(params[:id])
   end
