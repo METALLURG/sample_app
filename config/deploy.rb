b8b3f20b881160b3250d312911b0fe0effb6a326
Metallurg <met@iMac-Metallurg.local>
2015-07-07 18:09:16 +0300

    Deploy Ruby 2.0.0 test conf v.02

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,9 +14,20 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 
 # Ruby свистелки
 set :rbenv_type, :user
-set :rbenv_ruby, '2.1.5'
+set :rbenv_ruby, '2.0.0'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_roles, :all
 
 # А это рекомендуют добавить для приложений, использующих ActiveRecord
 set :puma_init_active_record, true
+
+desc 'Run rake tasks on server'
+task :rake do
+  on roles(:app), in: :sequence, wait: 5 do
+    within release_path do
+      with rails_env: :production do
+        execute :rake, ENV['task'], 'RAILS_ENV=production'
+      end
+    end
+  end
+end
